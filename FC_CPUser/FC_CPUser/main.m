cd260c7b5ce997375a204fbf1357db669e617e8e
ZhouYou <zhouyou@sobey.com>
2018-10-29 19:19:19 +0800

    添加测试项目

---
+++ FC_CPUser/FC_CPUser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FC_CPUser
+//
+//  Created by ZhouYou on 2018/10/29.
+//  Copyright © 2018 ZhouYou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
